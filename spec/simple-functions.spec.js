bb6c46d3d8cabe3256ebccb8c3eed11146a1f77d
PinguinoSod <pinguino.sod@gmail.com>
2019-08-08 22:47:15 +0200

    Add another function and tests

--- spec/simple-functions.spec.js
+++ spec/simple-functions.spec.js
@@ -1,7 +1,17 @@
-const { sum } = require('../src/simple-functions');
+const { sum, subtract } = require('../src/simple-functions');
 
 describe("sum", function() {
   it("should sum 2 and 3", function() {
     expect(sum(2, 3)).toBe(5);
+  });
+
+  it("should sum 3 and 2", function() {
+    expect(sum(3, 2)).toBe(5);
+  });
+});
+
+describe("subtract", function() {
+  it("should subtract 3 from 5", function() {
+    expect(subtract(5, 3)).toBe(2);
   });
 });
--- src/simple-functions.js
+++ src/simple-functions.js
@@ -2,8 +2,13 @@ const sum = function (a, b) {
   return a + b;
 }
 
+const subtract = function (a, b) {
+  return a - b;
+}
+
 function dontCallMe() {
   console.log('This should not be executed');
 }
 
 exports.sum = sum;
+exports.subtract = subtract;
